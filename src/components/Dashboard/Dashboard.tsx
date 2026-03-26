34a8fcc03d69fbbe78aa8ecd86db68aa725f5c89
Saumya <saumyasinha673@gmail.com>
2026-03-27 00:07:35 +0530

    feat: create simple to do

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,59 @@
-import "./Dashboard.css"
-import Button from "../Button/Button"
+import "./Dashboard.css";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [toDo, setToDo] = useState("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const handleClick = () => {
+    if (toDo.trim() === "") return;
+
+    setTasks([...tasks, toDo]);
+    setToDo("");
+  };
   return (
     <>
-      <div className="heading">Dashboard</div>
+      <div className="heading">My To Do</div>
       <div className="button">
-        <Button title="Create your notes"/>
-        <Button title="View your notes"/>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleClick();
+          }}
+        >
+          <input
+            value={toDo}
+            onChange={(e) => {
+              setToDo(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter your notes"
+          />
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    </>
-  )
-}
 
-export default Dashboard
+      {/* table */}
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {tasks.map((item, key) => {
+            return (
+              <tr key={key}>
+                <td>{item}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Dashboard;
